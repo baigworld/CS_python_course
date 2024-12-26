2cb6cd35df1330d6e8347faa1e8c244baf751c35
baigworld <mirzasaad.sas@gmail.com>
2024-12-25 23:47:24 -0500

    Create basic_loop_range.py

---
+++ basic_loop_range.py
@@ -0,0 +1,16 @@
+def main():
+    #print(" na na na na")
+    #print("na na na na")
+    for i in range(8):
+        print("na", end = " ")
+        print(i)
+    print("hey hey goodbye")
+    print(i)
+
+    i = 0
+    while i < 8:
+        print("na", end = " ")
+        i += 1
+    print("hey hey goodbye")
+
+main()
